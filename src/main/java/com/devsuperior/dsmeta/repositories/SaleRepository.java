81e5df88aeecb272807bd02322d0daeccc5b9190
Jonatas <jonatasmateuscontato@gmail.com>
2024-02-16 22:42:24 -0300

    GET report implemented

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.ReportDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
+import java.time.LocalDate;
+
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query(
+            "SELECT new com.devsuperior.dsmeta.dto.ReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) " +
+            "FROM Sale obj " +
+            "WHERE UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :sellerName, '%')) " +
+            "AND obj.date BETWEEN :minDate AND :maxDate " +
+            "ORDER BY obj.id"
+    )
+    Page<ReportDTO> getReport(LocalDate minDate, LocalDate maxDate, String sellerName, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.ReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +25,11 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<ReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
+		LocalDate newMaxDate = !maxDate.isEmpty() ? LocalDate.parse(maxDate) : LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate newMinDate = !minDate.isEmpty() ? LocalDate.parse(minDate) : newMaxDate.minusYears(1L);
+		return repository.getReport(newMinDate, newMaxDate, name, pageable);
 	}
 }
